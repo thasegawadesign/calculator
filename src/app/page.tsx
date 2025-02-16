5818ae55b31cca88b1804cae25ea0ba509eb250a
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-02-16 16:10:36 +0900

    fix: type change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,44 @@
 import { JSX, useState } from 'react';
 import { BsPlusSlashMinus } from 'react-icons/bs';
 
-type ButtonLabel = string | JSX.Element;
+type ButtonLabel = { name: string; content: string | JSX.Element };
 
 const line1: ButtonLabel[] = [
-  'AC',
-  <>
-    <BsPlusSlashMinus />
-  </>,
-  '%',
-  '÷',
+  { name: 'AC', content: 'AC' },
+  {
+    name: 'plusSlashMinus',
+    content: (
+      <>
+        <BsPlusSlashMinus />
+      </>
+    ),
+  },
+  { name: '%', content: '%' },
+  { name: '÷', content: '÷' },
 ];
-const line2: ButtonLabel[] = ['7', '8', '9', '×'];
-const line3: ButtonLabel[] = ['4', '5', '6', '-'];
-const line4: ButtonLabel[] = ['1', '2', '3', '+'];
-const line5: ButtonLabel[] = ['0', '.', '='];
+const line2: ButtonLabel[] = [
+  { name: '7', content: '7' },
+  { name: '8', content: '8' },
+  { name: '9', content: '9' },
+  { name: '×', content: '×' },
+];
+const line3: ButtonLabel[] = [
+  { name: '4', content: '4' },
+  { name: '5', content: '5' },
+  { name: '6', content: '6' },
+  { name: '-', content: '-' },
+];
+const line4: ButtonLabel[] = [
+  { name: '1', content: '1' },
+  { name: '2', content: '2' },
+  { name: '3', content: '3' },
+  { name: '+', content: '+' },
+];
+const line5: ButtonLabel[] = [
+  { name: '0', content: '0' },
+  { name: '.', content: '.' },
+  { name: '=', content: '=' },
+];
 
 export default function Home() {
   const [input, setInput] = useState('');
@@ -26,20 +50,20 @@ export default function Home() {
     <main>
       <p>{input || '0'}</p>
       <p>{result}</p>
-      {line1.map((char, i) => (
-        <button key={i}>{char}</button>
+      {line1.map((item) => (
+        <button key={item.name}>{item.content}</button>
       ))}
-      {line2.map((char, i) => (
-        <button key={i}>{char}</button>
+      {line2.map((item) => (
+        <button key={item.name}>{item.content}</button>
       ))}
-      {line3.map((char, i) => (
-        <button key={i}>{char}</button>
+      {line3.map((item) => (
+        <button key={item.name}>{item.content}</button>
       ))}
-      {line4.map((char, i) => (
-        <button key={i}>{char}</button>
+      {line4.map((item) => (
+        <button key={item.name}>{item.content}</button>
       ))}
-      {line5.map((char, i) => (
-        <button key={i}>{char}</button>
+      {line5.map((item) => (
+        <button key={item.name}>{item.content}</button>
       ))}
     </main>
   );
