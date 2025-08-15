a06a20757eed0d143a07398288ae0dd23aeb0e33
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-08-15 20:10:45 +0900

    fix: title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: '電卓',
+  title: "JavaScript電卓",
 };
 
 export default function RootLayout({
